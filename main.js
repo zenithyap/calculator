2063c659cd9694f82ac8c735e563d18a09533571
zenith <zenithyap88@gmail.com>
2025-01-06 09:13:07 +0800

    Add basic math operators

---
+++ main.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(a, b, op) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+    }
+}
