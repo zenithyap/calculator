fc2a9336cf9dd35f26b6546988439fb5360d2d2b
zenith <zenithyap88@gmail.com>
2025-01-09 10:02:19 +0800

    Add calculator logic

--- main.js
+++ main.js
@@ -30,3 +30,56 @@ function operate(a, b, op) {
             break;
     }
 }
+
+let a = "0";
+let b = "";
+let op = "";
+const buttons = document.querySelector("#buttons-container");
+const display = document.querySelector("#display");
+
+function reset(...args) {
+    for (const arg of args) {
+        switch (arg) {
+            case "a":
+                a = "0";
+                break;
+            case "b":
+                b = "";
+                break;
+            case "op":
+                op = "";
+                break;
+        }
+    }
+}
+
+buttons?.addEventListener("click", (event) => {
+    const target = event.target;
+    // Prevents event delgation of buttons-row
+    if (event.target.tagName === "BUTTON") {
+        const button = target.textContent;
+    
+        if (button === "+" || button === "-" || button === "*" || button === "/") {
+            if (a !== "" && b !== "") {
+                a = operate(+a, +b, op);
+                reset("b");
+            }
+            op = button;
+        } else if (button === "=") {
+            if (b !== "") {
+                a = operate(+a, +b, op);
+                reset("b");
+                op = "=";
+            }
+        } else if (button === "CLEAR") {
+            reset("a", "b", "op");
+        } else {
+            if (op === "=") {
+                reset("a", "op");
+            }
+            op === "" ? a += button : b += button
+        }
+    
+        display.textContent = b === "" ? +a : +b;
+    }
+});
